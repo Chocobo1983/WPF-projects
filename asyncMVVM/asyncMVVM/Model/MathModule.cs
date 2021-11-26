177850db6296317107dc6af2218afa0582c7ca78
Nickolay Gureyev <stb.infoserv@gmail.com>
2021-11-26 20:41:11 +0200

    Learning MVVM pattern with asynchronous methods
    MVVM with async-methods

---
+++ asyncMVVM/asyncMVVM/Model/MathModule.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace asyncMVVM.Model
+{
+    internal class MathModule
+    {
+        static double Factorial(int num)
+        {
+            double factorial = 1;
+            for (int i = 1; i <= num; i++) factorial *= i; 
+            return factorial;
+        }
+        public static double FactorialAsync(int n) => Task.Run(() => Factorial(n)).Result; 
+        public static double PowAsync(int x, int y) => Task.Run(() => Math.Pow(x, y)).Result;
+        public static int VowelsAsync(string str) => Task.Run(() => Regex.Matches(str, @"[уеыаоэяиёюAEIOU]", RegexOptions.IgnoreCase).Count).Result;
+        public static int ConsonantsAsync(string str) => Task.Run(() => Regex.Matches(str, @"[цкнгшщзхфвпрлджчсмтбBCDFGHJKLMNPQRSTVWXYZ]", RegexOptions.IgnoreCase).Count).Result;
+        public static int SymbolsAsync(string str) => Task.Run(() => Regex.Matches(str, "[~`!@№#$%^&*()_+=\\-?:;,./|''{}\"[]", RegexOptions.IgnoreCase).Count).Result;
+        public static int NumbersAsync(string str) => Task.Run(() => Regex.Matches(str, @"[0123456789]", RegexOptions.IgnoreCase).Count).Result;
+    }
+}
---
+++ asyncMVVM/asyncMVVM/ViewModel/VM.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asyncMVVM.ViewModel
+{
+    public class VM : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        private int _num;
+        public int Num { get { return _num; } set { _num = value; OnPropertyChanged("Factorial"); } }
+        public double Factorial => Model.MathModule.FactorialAsync(Num);
+        private int _num1;
+        public int Num1 { get { return _num1; } set { _num1 = value; OnPropertyChanged("Pow"); } }
+        private int _num2;
+        public int Num2 { get { return _num2; } set { _num2 = value; OnPropertyChanged("Pow"); } }
+        public double Pow => Model.MathModule.PowAsync(Num1, Num2);
+        private string? _str1;
+        public string Str1 { get { return _str1 ?? ""; } set { _str1 = value; OnPropertyChanged("Vowels"); } }
+        public string Vowels => $"Vowels: {Model.MathModule.VowelsAsync(Str1)}\tConsonants: {Model.MathModule.ConsonantsAsync(Str1)}\nSymbols: {Model.MathModule.SymbolsAsync(Str1)}\tNumbers: {Model.MathModule.NumbersAsync(Str1)}";
+        
+
+    }
+}
